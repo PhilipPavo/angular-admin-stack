a9ab6ac77dc55953bc22c6e4a7fcf71bafabc04c
Philip Pavo <catl33t@gmail.com>
2017-07-08 23:31:17 +0300

    angular 2 + semantic ui deploy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AdminStack
+# admin-panel-stack Frontend (Angular 2 + Semantic UI)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SuiModule} from 'ng2-semantic-ui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SuiModule,
     BrowserModule
   ],
   providers: [],
